d89086339aeaf622c8922cd4111068287d00de76
George Whiteside <george@interon.co.za>
2022-02-17 13:08:03 +0200

    Broken Solution

--- Interon.Roadlab.Web/App/ViewModels/ContactModel.cs
+++ Interon.Roadlab.Web/App/ViewModels/ContactModel.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
+ 
 using System.Linq;
 using System.Web.UI.WebControls;
 
