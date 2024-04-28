d79b0022f50c35f03f960156f92e86d5d9a4f4fa
Anton <anton@interon.co.za>
2024-04-28 12:50:38 +0200

    Fix search

--- Interon.Roadlab.Web.v13/Startup.cs
+++ Interon.Roadlab.Web.v13/Startup.cs
@@ -1,5 +1,6 @@
 using Interon.Roadlab.Web.Net.Core.Config;
 using Interon.Roadlab.Web.Net.Core.Controllers;
+using Our.Umbraco.FullTextSearch;
 
 namespace Interon.Roadlab.Web.v13
 {
@@ -35,10 +36,11 @@ namespace Interon.Roadlab.Web.v13
         {
             services.Configure<EmailSettings>(_config.GetSection("EmailSettings"));
 	        services.AddHttpContextAccessor();
-            services.AddTransient<ISearchService, SearchService>();
+            
             services.AddUmbraco(_env, _config)
                 .AddBackOffice()
                 .AddWebsite()
+                 
                 .AddComposers()
                 .Build();
         }
